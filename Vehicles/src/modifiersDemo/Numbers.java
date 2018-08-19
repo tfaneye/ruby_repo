a5b53ba3748d965a06df119b40068acabfa94f89
tfaneye <tunde_eye@yahoo.com>
2018-08-19 19:55:22 +0100

    added notes to the Class Notes and added codes that calculates my daily wages

---
+++ Vehicles/src/modifiersDemo/Numbers.java
@@ -0,0 +1,15 @@
+package modifiersDemo;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		int workingHour = 8;
+		double rate = 10.0;
+		
+		double salary = workingHour * rate;
+		
+		System.out.println("My salary for a day is = £" + salary);
+
+	}
+
+}
