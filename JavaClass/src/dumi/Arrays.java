8c49fe8170a5bafb9db3166884f8e94a4c860d43
dumi6 <dumi6@outlook.com>
2018-09-10 20:25:16 +0100

    Creare Java clss Project

---
+++ JavaClass/src/dumi/Arrays.java
@@ -0,0 +1,36 @@
+package dumi;
+
+public class Arrays {
+
+
+    public static void main(String [] args){
+
+        String [] fruits = {"banana", "apple", "orange", "mango"};
+
+        System.out.println("All the values in the array are: ");
+
+        for (int i = 0; i < fruits.length; i++){
+
+            System.out.println(fruits [i]);
+
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
